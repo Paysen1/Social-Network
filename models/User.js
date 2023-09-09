deca1fa3cd6dc0e7ed7e6fb5ae1ee048652ac809
Peak <pglund18@yahoo.com>
2023-09-09 03:27:34 -0500

    user.js

--- models/User.js
+++ models/User.js
@@ -1,5 +1,36 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    
-})
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+      },
+      thoughts: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Thought',
+        },
+      ],
+      friends: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User',
+        },
+      ],
+});
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.lengh;
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
