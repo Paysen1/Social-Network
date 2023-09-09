57d14821f5c1cc44dec4dd1aea0b545a6d9bea6b
Peak <pglund18@yahoo.com>
2023-09-09 03:47:16 -0500

    Reaction.js and added route to thoughts

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,5 +1,21 @@
 const mongoose = require('mongoose')
 
 const ReactionSchema = new mongoose.Schema({
-    
-})
+    reactionId: mongoose.Schema.Types.ObjectId,
+    reactionBody: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 220,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    timeCreated: {
+        type: Date,
+        defult: Date.now,
+    },
+});
+
+module.exports = ReactionSchema;
--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const reactionSchema = require('./Reaction');
 
 const ThoughtSchema = new mongoose.Schema({
     thoughtText: {
