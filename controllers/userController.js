a1dd89096ca673f2e00bc6b0270f6223af85ce15
Peak <pglund18@yahoo.com>
2023-09-10 06:33:50 -0500

    starting userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,21 @@
 const User = require('../models/User');
 
 //getAllUsers
-
+const getAllUsers = async (req, res) => {
+    try{
+        const users = await User.find().populate('thoughts friends');
+        res.json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Server error'});
+    }
+};
 //getUserById
-
+const getUserById = async (req, res) => {
+    try{
+        
+    }
+}
 //createUser
 
 //updateUser
